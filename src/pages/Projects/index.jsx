4f01c9d4d89ad691fb8cce76b43a2a8b44e04a94
lobomanz <lovro.monkey@gmail.com>
2025-12-05 21:04:10 +0100

    added marrgin

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -2,18 +2,21 @@ import { useParams } from "react-router-dom";
 import { useMemo } from "react";
 import Gateway from "../../Gateway";
 import ProjectList from "./components/ProjectList";
+import { StyledProjects } from "./styledProjects";
 
 export default function Projects() {
-    const { siteName } = useParams();
+  const { siteName } = useParams();
 
-    const projectList = useMemo(() => {
-        if (!siteName) return [];
-        return Gateway.getSiteProjects(siteName);
-    }, [siteName]);
+  const projectList = useMemo(() => {
+    if (!siteName) return [];
+    return Gateway.getSiteProjects(siteName);
+  }, [siteName]);
 
-    return (
-        <>
-            <ProjectList projectList={projectList} />
-        </>
-    );
+  return (
+    <>
+      <StyledProjects>
+        <ProjectList projectList={projectList} />
+      </StyledProjects>
+    </>
+  );
 }
--- src/pages/Projects/styledProjects.js
+++ src/pages/Projects/styledProjects.js
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
 
-export const StyledHomepage = styled.div`
+export const StyledProjects = styled.div`
     max-width: 100vw;
     height: 100%;
     margin-top: 60px;
